3a82b0f8cca91c94da7b56943b38137eeb7477d0
Rimix-incognito <75817909+Rimix-incognito@users.noreply.github.com>
2022-04-04 14:49:41 +0100

    --

--- src/components/Blogdetails.js
+++ src/components/Blogdetails.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useFetch from '../customhooks/useFetch';
 
 const Blogdetails = () => {
     const {id} =useParams();
+    const {data:blog, isloading, error} = useFetch(`http://localhost:4000/blogs/${id}`);
   return (
     <div className='blog-details'>
-        <h2>Blog Details - {id}</h2>
+        {
+            isloading && <div>Loading.....</div>
+        }
+        {
+            error && <div>{error}</div>
+        }
+        {
+            blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )
+        }
+       
     </div>
   )
 }
--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import {useState} from 'react'
 
 export const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('rimi');
+
+  const submit = (e) => {
+      e.preventDefault();
+      const blog = {title, body, author};
+      fetch('http://localhost:4000/blogs', {
+        method:'POST',
+        headers:{'content-Type':'Application/json'},
+        body:JSON.stringify(blog)
+      }).then(() => {
+        console.log('new blog added');
+      })
+  }
+
   return (
     <div className='create'>
         <h2>Add a new blog</h2>
+        <form onSubmit={submit}>
+          <label>Blog Title:</label>
+          <input 
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          />
+           <label>Blog body:</label>
+         <textarea required value={body} onChange={(e) => setBody(e.target.value)}/>
+         <label>Blog author</label>
+          <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+            <option value="rimi">rimi</option>
+            <option value="ife">ife</option>
+          </select>
+          <button>Add blog</button>
+        </form>
     </div>
   )
 }
